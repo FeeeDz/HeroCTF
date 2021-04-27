e2a223d9dc7c216bebaa8702e85b7eaef97b6c3c
FeeeDz <67475596+FeeeDz@users.noreply.github.com>
2021-04-27 17:18:31 +0200

    Update ping_pong.py

--- Ping Pong/ping_pong.py
+++ Ping Pong/ping_pong.py
@@ -1,13 +1,11 @@
-f = open("out.txt",'r')
-#print(f.read())
-bin=''
+f = open("output.txt",'r')
+binary=''
 for i in range(0,176):
     linea = f.readline()
-    #print("Linea---> "+linea)
     if linea == "PONG\n":
-        bin+='0'
+        binary+='0'
     elif linea == "PING\n":
-        bin+='1'
-print(bin)
+        binary+='1'
+print(binary)
 # flag --> Hero{p1n6_p0n6_15_fun}
 # https://gchq.github.io/CyberChef/#recipe=From_Binary('None',8)&input=MDEwMDEwMDAwMTEwMDEwMTAxMTEwMDEwMDExMDExMTEwMTExMTAxMTAxMTEwMDAwMDAxMTAwMDEwMTEwMTExMDAwMTEwMTEwMDEwMTExMTEwMTExMDAwMDAwMTEwMDAwMDExMDExMTAwMDExMDExMDAxMDExMTExMDAxMTAwMDEwMDExMDEwMTAxMDExMTExMDExMDAxMTAwMTExMDEwMTAxMTAxMTEwMDExMTExMDE
